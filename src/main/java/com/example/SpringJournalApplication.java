111781a29b97d2c4c588f272a3328e412c06fbf0
abdullah <aaa006bd@gmail.com>
2016-06-19 18:21:20 +0600

    unused header removed

--- src/main/java/com/example/SpringJournalApplication.java
+++ src/main/java/com/example/SpringJournalApplication.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
